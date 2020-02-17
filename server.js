bc3cbdc2026f4a8f58e90c176d8ac123e6b05821
Daniel Mattox <dmattox10@gmail.com>
2020-02-17 14:12:17 -0500

    Finished with server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const accountRouter = require('./account-router')
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountRouter)
+
+server.get('/', (req, res) => {
+    res.send('<h2>“The code is more what you’d call ‘guidelines’ than actual rules.” – Hector Barbossa</h2>')
+})
 
 module.exports = server;
